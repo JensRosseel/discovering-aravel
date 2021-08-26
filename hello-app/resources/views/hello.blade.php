36e649257a7735bb0ab276baba15e57a4480641d
JensRosseel <slaythehorizon.games@gmail.com>
2021-08-26 15:08:05 +0200

    Add sass styling with compiler

--- hello-app/resources/views/hello.blade.php
+++ hello-app/resources/views/hello.blade.php
@@ -1,2 +1,14 @@
-<h1>Hello World!!!</h1>
-<a href="{{ route('hello2') }}">To 2</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/app.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello World!!!</h1>
+    <a href="{{ route('hello2') }}">To 2</a>
+</body>
+</html>
